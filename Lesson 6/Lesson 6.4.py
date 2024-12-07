b3148fae0ad7a292679aa7a04e606b4105403c4d
hytsai19 <vivian3811@gmail.com>
2024-12-07 16:22:28 +0800

    modify

--- Lesson 6/Lesson 6.4.py
+++ Lesson 6/Lesson 6.4.py
@@ -18,3 +18,12 @@ with st.sidebar:
     )
 
     st.title(f"{add_selectbox}")
+
+allData:list[dict] = tools.get_aqi(excel_name="aqi.xlsx")
+#selected_item:[dict]=[]
+#for item in allData:
+    #if item["sitename"] == add_selectbox:
+        #selected_item.append(item)
+
+selected_item:list[dict] = [item for item in allData if item['sitename']==add_selectbox]
+st.table(data=selected_item)
