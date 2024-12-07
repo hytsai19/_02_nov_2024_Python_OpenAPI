c72d6e00c103a5367edd6ccf4a47ecec0c7f98bf
hytsai19 <vivian3811@gmail.com>
2024-12-07 15:48:18 +0800

    modify

--- Lesson 6/Lesson 6.4.py
+++ Lesson 6/Lesson 6.4.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import tools
 
+sitenames:list[str]= tools.get_sitenames(excel_name="aqi.xlsx")
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+   " 請選擇站點名稱",
+    sitenames
 )
-
+#檔名不要有空格
+#run prgramme要從terminal下command: streamlit run "file_name.py"
