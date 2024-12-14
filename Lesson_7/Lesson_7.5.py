7f8880536adfb618d147b9c39553a542535caaad
hytsai19 <vivian3811@gmail.com>
2024-12-14 16:19:00 +0800

    modify

--- Lesson_7/Lesson_7.5.py
+++ Lesson_7/Lesson_7.5.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-if "key" not in st.session_state:
-    st.session_state["key"]="value"
-    
+st.title("次數的範例")
+if 'count' not in st.session_state:
+    st.session_state['count']=0
+
+increament = st.button('每次加1')
+if increament:
+    st.session_state['count'] += 1
+
+st.write('次數=',st.session_state['count'])
