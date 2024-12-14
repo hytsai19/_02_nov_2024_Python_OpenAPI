e1e7a6b123be516e8f947c05fab0b2ae56177852
hytsai19 <vivian3811@gmail.com>
2024-12-14 15:38:02 +0800

    modify

--- Lesson_7/Lesson7_3.py
+++ Lesson_7/Lesson7_3.py
@@ -1,15 +1,15 @@
-from .import taipei
+from tools import taipei
 import streamlit as st
 
-@st.dialog("Cast your vote)
-def vote(item):
+@st.dialog("目前網路出現狀況")
+def vote(error):
     st.write(error)
 
 try:
     youbike_data:list[dict] = taipei.get_youbikes()
-except Exception as e:
-    print(e)
-    st.stop
-else:
-    print(youbike_data)
+except Exception as error:    
+    vote(error)
+    st.write("喝杯咖啡!請等一下再試!")
+    st.stop()
 
+st.table(youbike_data)
