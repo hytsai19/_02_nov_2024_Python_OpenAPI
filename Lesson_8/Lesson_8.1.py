8b5b370243fcf38517ce672ff98255c3bf1ae1f7
hystsai19 <vivian3811@gmail.com>
2024-12-21 10:28:06 +0800

    modify

--- Lesson_8/Lesson_8.1.py
+++ Lesson_8/Lesson_8.1.py
@@ -1,12 +1,12 @@
+import streamlit as st
 
-import streamlity as st
-
-#手動建立counter_key，並設定初始值為0 (寫在dictionary中只會執行一次，稱為初始化)
+#手動建立counter_key, 並設定初始值為0
 if "counter" not in st.session_state:
-    #st.session_state["counter"]=0
-    st.session_state.counter=0
+    #st.session_state['counter'] = 0
+    st.session_state.counter = 0
 
-st.sessopm_state.counter +=1
 
-st.header(f"這頁網頁已經執行"{st.session_state.copunter}次")
-st.button("再執行一次", key="reset")
+st.header(f"這頁網頁已經執行{st.session_state.counter} 次")
+buttet_status = st.button("再執行一次", key="reset")#自動建立reset_key
+if buttet_status:
+    st.session_state.counter += 1
